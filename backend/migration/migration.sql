09b7e0f76c30e5312505466606fb784ea2b68595
Fırat Kırbay <frtchngrkrby@gmail.com>
2021-09-09 13:00:09 +0300

    migration added

---
+++ backend/migration/migration.sql
@@ -0,0 +1,30 @@
+CREATE TABLE `products` (
+  `id` varchar(100) PRIMARY KEY,
+  `title` varchar(300) DEFAULT NULL,
+  `description` varchar(3000) DEFAULT NULL,
+  `link` varchar(1000) DEFAULT NULL,
+  `image_link` varchar(100) DEFAULT NULL,
+  `availability` varchar(100) DEFAULT NULL,
+  `price` varchar(100) DEFAULT NULL,
+  `brand` varchar(100) DEFAULT NULL,
+  `gtin` varchar(100) DEFAULT NULL,
+  `mpn` varchar(100) DEFAULT NULL,
+  `condition` varchar(100) DEFAULT NULL,
+  `shipping` varchar(100) DEFAULT NULL,
+  `identifier_exists` varchar(100) DEFAULT NULL,
+  `google_product_category` varchar(100) DEFAULT NULL,
+  `adult` varchar(100) DEFAULT NULL,
+  `gender` varchar(100) DEFAULT NULL,
+  `color` varchar(100) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+--
+-- Tablo döküm verisi `products`
+--
+
+INSERT INTO `products` (`id`, `title`, `description`, `link`, `image_link`, `availability`, `price`, `brand`, `gtin`, `mpn`, `condition`, `shipping`, `identifier_exists`, `google_product_category`, `adult`, `gender`, `color`) VALUES
+('25a7498d-2e25-4fe8-9ff9-baef3df773ea', 'Multi-Color Driehoek 5W LED Wall Sconce Lamp Omhoog & Omlaag Binnenverlichting', 'Kenmerken:Populaire moderne driehoeksontwerp, kostuum voor thuis, bar, koffiewinkel, binnen- en buitendecoratie.Geen UV of IR stralingLaag energieverbruik,Gemakkelijk te installeren, gewoon te vervangen, handig te gebruiken.Gemaakt van aluminium, hoogwaardig en duurzaam.Specificatie:Type: Driehoek LED wandlampIngang: AC 85-265V 50 / 60HzVermogen: 5WMateriaal: AluminiumHoeveelheid: 1 stuksItem Kleur: Zilver, zwartLichtgevend: Flux 400LMLichte Kleur: Kleurrijk, 5 Kleur: Paars, Blauw, Groen, Rood, Warm WitDimbaar: NeeBreedtehoek: 120 gradenWaterdicht: NeeItem Grootte - 23,5 * 11,7 * 3cm / 9,2 * 4,6 * 1,2 Pakket Inbegrepen:1 * Driehoek LED wandlamp', 'https://www.dennisdeal.com/products/multi-color-driehoek-5w-led-wall-sconce-lamp-omhoog-omlaag-binnenverlichting_1080112?variant=31733545467994', 'https://dnqslujgppusp.cloudfront.net/products/Dy4kH81ayzF.jpg', 'in stock', '20.99', 'DennisDeal', '', '', 'new', 'NL:::0.00 EUR', 'false', '', '', '', ''),
+('da4a6373-30db-46bc-9116-e165bcc0d46b', 'One Para 22mm rem- en koppelingshefboom voor 110cc 125cc 140cc 160cc Pit Dirt Bike', 'Rem- en koppelingshefboom voor 110cc 125cc 140cc 160cc Pit Dirt BikeBeschrijving:Snel terugremmen en koppelingshefbomen zijn veerladen zodat ze minder kans hebben om te breken bij het botsen met de grond.Fitments: Voor de meeste 110cc 125cc 140cc 160cc PitfietsenNotitie:1. Controleer alstublieft de maatmetingdiagram voordat u de betaling doet.2. Laat het verschil van 0,5-1 inch door manuele meting. (1 inch = 2,54cm)3. De kleur van de werkelijke items kan iets afwijken van de lijst met afbeeldingen Door een ander computerscherm, bedankt voor uw begrip.4. Instructie is niet inbegrepen.specificaties:Materiaal: MetaalDe kleur zwartLengte van de remhendel: ongeveer 119mmAfgewerkt in zwart en ontworpen om passen standaard 22mm staafjes .Verstelbare pre sets maken de persoonlijke instelling van de hendel mogelijk om verschillende handformaten aan te passen.Pakket Inbegrepen:1 x paar remhendel koppeling', 'https://www.dennisdeal.com/products/one-para-22mm-rem-en-koppelingshefboom-voor-110cc-125cc-140cc-160cc-pit-dirt-bike_981241?variant=31740765798490', 'https://dnqslujgppusp.cloudfront.net/products/ei01lz5EAzN.jpg', 'in stock', '15.99', 'DennisDeal', '', '', 'new', 'NL:::0.00 EUR', 'false', '', '', '', ''),
+('bbb1eae0-1d6b-4828-8f8f-2f650c73e5c9', '20 stks 3.7 V 4.2 V 18650 Lithium Leeuw Batterij Bescherming Board Charger Ontlading Bescherm DD04CPMA', 'Kenmerken: Bescherming tegen overbelastingOverontlading beschermingKortsluitingsbeveiligingOverstroombeveiligingBescherm een enkele 3,7 V lithiumbatterijBescherm meerdere 3,7 V lithiumbatterijen (niet meer dan 10 aanbevolen) Parameter: Overlaad detectiespanning: 4,28 Â± 0,05 VOverlaadvrijgavespanning: 4,26 Â± 0,05 VDetectie spanning overontlading: 2.5 Â± 0.1VOverstroom detectiestroom: 1-3AOverstroombeveiliging: 3,2 Â± 0,2AAfmeting: 12.6x10.2x2.1mmGewicht: 0,34 g Opmerking: laad opnieuw op en activeer de beveiligingskaart. Anders is er geen spanningsuitgang bedrading: B-aangesloten op de batterij negatiefB + aangesloten batterij positiefP + aansluitlader / belasting positiefP-connector lader / belasting negatief Toepassing: 3,7 V lithiumbatterij 18650TP4056 laadmodule Verpakking omvat: 20 x 3,7 V 4,2 V 3A opladerbeveiligingskaart', 'https://www.dennisdeal.com/products/20-stks-3-7-v-4-2-v-18650-lithium-leeuw-batterij-bescherming-board-charger-ontlading-bescherm-dd04cpma_1577841?variant=31734739435610', 'https://dnqslujgppusp.cloudfront.net/products/PTRSpzDd4bC.jpg', 'in stock', '31.99', 'DennisDeal', '', '', 'new', 'NL:::0.00 EUR', 'false', '', '', '', ''),
+('ea23f85a-6bd1-404f-90fa-c0faadf007c1', '10st Female Head RS232 Turn Terminal SeriÃ«le Poort Adapter DB9 Terminal Connector', 'Omschrijving: Module Gebruik: DB9 adapteraansluiting, handig voor engineering, on-site debugging apparatuur gebruik Bordformaat: 4,0 cm x 3,1 cm Pakket inbegrepen: 10 x Female Head RS232 Turn Terminal SeriÃ«le Poort', 'https://www.dennisdeal.com/products/10st-female-head-rs232-turn-terminal-seriele-poort-adapter-db9-terminal-connector_1429347?variant=31734743302234', 'https://dnqslujgppusp.cloudfront.net/products/Ouyjx6fyOWQ.jpg', 'in stock', '31.99', 'DennisDeal', '', '', 'new', 'NL:::0.00 EUR', 'false', '', '', '', '');
+COMMIT;
